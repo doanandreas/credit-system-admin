65c0252c053bc7c23da503fc3fc986eea9f6398b
doanandreas <doanan3736@gmail.com>
2023-02-08 23:59:54 +0700

    implement withdraw

--- controllers/credit.js
+++ controllers/credit.js
@@ -26,3 +26,25 @@ exports.deposit = asyncHandler(async (req, res, next) => {
     balance: updated.balance.toString(),
   });
 });
+
+// @desc	  Withdraw user's money
+// @route	  POST /credit/withdraw
+// @access	Private
+exports.withdraw = asyncHandler(async (req, res, next) => {
+  const { amount } = req.body;
+
+  const user = await prisma.user.findUnique({
+    where: { username: req.user.username },
+  });
+
+  const updated = await prisma.user.update({
+    where: { username: req.user.username },
+    data: { balance: user.balance - BigInt(amount) },
+  });
+
+  res.status(200).json({
+    success: true,
+    name: user.name,
+    balance: updated.balance.toString(),
+  });
+});
--- routes/credit.js
+++ routes/credit.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
-const { deposit } = require("../controllers/credit");
+const { deposit, withdraw } = require("../controllers/credit");
 const { protectedRoute } = require("../middlewares/auth");
 
 const router = express.Router();
 
 router.post("/deposit", protectedRoute, deposit);
+router.post("/withdraw", protectedRoute, withdraw);
 
 module.exports = router;
