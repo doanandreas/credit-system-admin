24694d34c1475cffd66b2a696a71dd3b72b04756
doanandreas <doanan3736@gmail.com>
2023-02-08 22:50:11 +0700

    default balance for user

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,6 +6,17 @@ const ErrorResponse = require("../utils/errorResponse");
 
 const prisma = new PrismaClient();
 
+// @desc	  Register new user
+// @route	  POST /auth/register
+// @access	Public
+exports.register = asyncHandler(async (req, res, next) => {
+  const { name, username } = req.body;
+
+  
+
+  res.status(200).json({ success: true });
+});
+
 // @desc	  Log in
 // @route	  POST /auth/login
 // @access	Public
@@ -17,6 +28,12 @@ exports.login = asyncHandler(async (req, res, next) => {
       username,
     },
   });
+
+  if (!user)
+    throw new ErrorResponse(
+      "Invalid credentials. Please try a different username",
+      401
+    );
 
   const token = jwt.sign({ username }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRE,
