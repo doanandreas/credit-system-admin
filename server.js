910fc0d439d41e6ad1796a127e3b23984c7f1d3b
doanandreas <doanan3736@gmail.com>
2023-02-11 17:56:23 +0700

    sync sequelize w/ db when starting app

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 
+const sequelize = require("./models/db");
 const router = require("./routes");
 const errorHandler = require("./middlewares/error");
 
@@ -20,4 +21,21 @@ app.use(router);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, console.log(`Server running on port ${PORT}`));
+
+async function startServer() {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+
+    await sequelize.sync({ alter: true });
+    console.log("Database synchronized");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Error:", err);
+  }
+}
+
+startServer();
