25e23835abbaf8ba07f6d0c24c334e4056ee0f4b
doanandreas <doanan3736@gmail.com>
2023-02-11 18:18:30 +0700

    create model for user

---
+++ models/User.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("./db");
+
+const User = sequelize.define("User", {
+  username: {
+    type: DataTypes.STRING,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  balance: {
+    type: DataTypes.BIGINT,
+    allowNull: false,
+    defaultValue: 0,
+  },
+});
+
+module.exports = User;
