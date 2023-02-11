6264c90c64955c20b939faba4744ad7eb6939c74
doanandreas <doanan3736@gmail.com>
2023-02-11 18:54:43 +0700

    add car model

---
+++ models/Car.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("./db");
+
+const Car = sequelize.define("Car", {
+  brandName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  groupModelName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  modelName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  year: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.BIGINT,
+    allowNull: false,
+  },
+  fullName: {
+    type: DataTypes.VIRTUAL,
+    get: () => `${this.brandName} ${this.groupModelName} ${this.modelName}`,
+    set: () => throw new Error("Virtual attribute 'fullName' not to set"),
+  },
+});
+
+module.exports = Car;
