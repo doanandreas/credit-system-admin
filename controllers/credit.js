d90a9cb97b9008a9ee1d8a8046b2d1fa977e194a
doanandreas <doanan3736@gmail.com>
2023-02-11 19:52:02 +0700

    migrate purchase car endpoint

--- controllers/credit.js
+++ controllers/credit.js
@@ -1,6 +1,6 @@
 const { PrismaClient } = require("@prisma/client");
 
-const { User } = require("../models");
+const { User, Car, Leasing, CarPurchase } = require("../models");
 const asyncHandler = require("../utils/asyncHandler");
 const ErrorResponse = require("../utils/errorResponse");
 
@@ -42,37 +42,33 @@ exports.withdraw = asyncHandler(async (req, res, next) => {
 // @route	  POST /credit/purchase
 // @access	Private
 exports.purchase = asyncHandler(async (req, res, next) => {
-  const { carId, leasingId, durationInMonths } = req.body;
+  const { carId, leasingId, creditDuration } = req.body;
 
-  const car = await prisma.car.findUnique({
-    where: { id: parseInt(carId) },
-  });
+  const car = await Car.findByPk(carId);
 
   if (!car) throw new ErrorResponse(`Car not found with ID ${carId}`, 400);
 
-  const leasing = await prisma.leasing.findUnique({
-    where: { id: parseInt(leasingId) },
-  });
+  const leasing = await Leasing.findByPk(leasingId);
 
   if (!leasing)
     throw new ErrorResponse(`Leasing not found with ID ${leasingId}`, 400);
 
-  const carPurchase = await prisma.carPurchase.create({
-    data: {
-      userId: req.user.id,
-      carId,
-      leasingId,
-      creditDuration: durationInMonths,
-    },
+  const carPurchase = await CarPurchase.create({
+    creditDuration,
+    UserUsername: req.user.username,
+    CarId: carId,
+    LeasingId: leasingId,
   });
 
   res.status(200).json({
     success: true,
-    carPurchaseId: carPurchase.id,
-    customer: req.user.name,
-    leasing: leasing.leasingName,
-    car: `${car.brandName} ${car.groupModelName} ${car.modelName}`,
-    price: car.price.toString(),
+    data: {
+      id: carPurchase.id,
+      customer: req.user.name,
+      leasing: leasing.name,
+      car: car.fullName,
+      price: car.price.toString(),
+    },
   });
 });
 
