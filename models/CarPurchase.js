14afd1704c23cb026ae9c6cb42973908cf56de31
doanandreas <doanan3736@gmail.com>
2023-02-12 13:25:15 +0700

    make car purchase soft delete

--- models/CarPurchase.js
+++ models/CarPurchase.js
@@ -1,11 +1,15 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("./db");
 
-const CarPurchase = sequelize.define("CarPurchase", {
-  creditDuration: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
+const CarPurchase = sequelize.define(
+  "CarPurchase",
+  {
+    creditDuration: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   },
-});
+  { paranoid: true }
+);
 
 module.exports = CarPurchase;
